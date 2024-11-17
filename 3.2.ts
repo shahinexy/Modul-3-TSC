b0f8ed4d5dd410f0390b28775ec0d0bae34a9044
shahinexy <shahinexy@gmail.com>
2024-11-17 22:28:27 +0600

    OOP Inheritance

---
+++ 3.2.ts
@@ -0,0 +1,23 @@
+{
+    // OOP - inheritance
+    class Student {
+        public name : string;
+        public age : number;
+        public address: string
+
+
+        constructor( name: string,  age: number,  address: string){
+            this.name = name;
+            this.age = age;
+            this.address =  address
+        }
+
+       
+
+    }
+
+    const student1 = new Student('Sakibut', 22, "Adamjee")
+
+
+    //
+}
