16d6e00c05bdc6a1244ffc46eda3b92e51565195
shahinexy <shahinexy@gmail.com>
2024-12-07 18:08:15 +0600

    in guard

--- src/3.3.ts
+++ src/3.3.ts
@@ -1,8 +1,9 @@
 {
     // Type guard
 
-    // typeof
-    const add = (param1: string | number, param2: string|number): string | number =>{
+    // typeof guard
+    type Alphaneumaric = string | number;
+    const add = (param1: Alphaneumaric, param2: Alphaneumaric): Alphaneumaric =>{
         if(typeof param1 === "number" && typeof param2 === "number"){
             return param1 + param2;
         }
@@ -13,6 +14,38 @@
 
     console.log(add(5,5));
     console.log(add('2',2));
+
+
+    // in guard
+    type NormalUser = {
+        name : string
+    }
+
+    type Admin = {
+        name: string,
+        role: "admin"
+    }
+
+    const getUser = (user: NormalUser | Admin) =>{
+        if('role' in user) {
+            return `My name is: ${user.name}, Role is: ${user.role}`
+        }
+        else{
+            return `My name is: ${user.name}`
+        }
+    }
+
+    const normalUser : NormalUser ={
+        name: "Rifat"
+    }
+    console.log(getUser(normalUser));
+
+    const admin : Admin ={
+        name: "Shahin",
+        role: "admin"
+    }
+    console.log(getUser(admin));
+    
 
     // 
 }
